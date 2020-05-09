281e60581521b8e84d85537c2e2b6431d3b086ea
urheilunen <quarussique@yandex.ru>
2020-05-10 00:07:24 +0500

    Added form to choose restaurant and week on app.html Fixed registration form Added logout page and function Added user menu Added Day class Some variables refactored Added function for logout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request
 import pickle
 from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
 
 
 # defining user class
@@ -8,6 +9,64 @@ class User:
     def __init__(self, name, password):
         self.name = name
         self.password = generate_password_hash(password)
+
+    def check_password(self, password_to_be_checked):
+        return check_password_hash(self.password, password_to_be_checked)
+
+
+# defining schedule classes
+class Day:
+    def __init__(self, date):
+        self.date = date
+        self.shifts = [
+            [[None, None], [None]],
+            [[None], [None]],
+            [[None], [None]],
+            [[None], [None]],
+            [[None], [None]]
+        ]
+
+    def __str__(self):
+        info = str(self.date)
+        for i in self.shifts:
+            info += '\n' + str(i)
+        return info
+
+
+class Week:
+    def __init__(self):
+        self.begin_date = datetime.datetime.today().date()
+        while self.begin_date.isoweekday() != 1:
+            self.begin_date += datetime.timedelta(days=1)
+        self.end_date = self.begin_date + datetime.timedelta(days=6)
+        self.schedule = [
+            Day(self.begin_date),
+            Day(self.begin_date + datetime.timedelta(days=1)),
+            Day(self.begin_date + datetime.timedelta(days=2)),
+            Day(self.begin_date + datetime.timedelta(days=3)),
+            Day(self.begin_date + datetime.timedelta(days=4)),
+            Day(self.begin_date + datetime.timedelta(days=5)),
+            Day(self.begin_date + datetime.timedelta(days=6))
+        ]
+        self.is_approved = False
+
+    def approve(self):
+        self.is_approved = True
+
+    def get_date(self):
+        return str(self.begin_date.strftime('%d/%m/%Y')) + '-' + str(self.end_date.strftime('%d/%m/%Y'))
+
+    # restaurant 0-4: pf-kt
+    # shift 0/1: morning/evening
+    def add_vacancy(self, day, restaurant, shift):
+        self.schedule[day][restaurant][shift].append(None)
+
+    def remove_vacancy(self, day, restaurant, shift):
+        self.schedule[day][restaurant][shift].remove(None)
+
+    def __str__(self):
+        for i in self.schedule:
+            print(i)
 
 
 # extracting database from file
@@ -48,12 +107,12 @@ def save_schedule():
 
 
 app = Flask(__name__)
-CURRENT_USER = False
+current_user = False
 
 
 @app.route('/', methods=['post', 'get'])
 def landing():
-    global CURRENT_USER
+    global current_user
     global users_database
     username = False
     password = False
@@ -66,26 +125,78 @@ def landing():
             if i.name == username:
                 user_found = True
                 if i.check_password(password):
-                    CURRENT_USER = i
+                    current_user = i
                     # successful authorization
-                    return render_template('app.html', username=username)
+                    return render_template('user_menu.html', username=username)
                 else:
                     # wrong password
                     return render_template('index.html', message='Wrong password', login=True)
         if not user_found:
-            return render_template('login.html', message='No such user')
+            return render_template('index.html', message='No such user', login=True)
     else:
-        return render_template('login.html', message='Type in correct login and password')
-    if not CURRENT_USER:
         return render_template('index.html', login=True)
-    else:
-        return render_template('index.html', user=CURRENT_USER)
+    # if not CURRENT_USER:
+    #     return render_template('index.html', login=True)
+    # else:
+    #     return render_template('index.html', user=CURRENT_USER)
 
 
 @app.route('/reg/', methods=['post', 'get'])
 def landing_reg():
-    return render_template('index.html', register=True)
+    global users_database
+    username = False
+    password1 = False
+    password2 = False
+    if request.method == 'POST':
+        username = request.form.get('username')  # запрос к данным формы
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+    if username and password1 and password2:
+        if password1 == password2:
+            # input users_data alright, begin working with database
+            user_found = False
+            for i in users_database:
+                if i.name == username:
+                    user_found = True
+                    break
+            if not user_found:
+                users_database.append(User(username, password1))
+                save_database()
+                return render_template('index.html', message='Successfully registered!', login=True)
+            else:
+                return render_template('index.html', message='User already exists', register=True)
+        else:
+            return render_template('index.html', message="Passwords don't match!", register=True)
+    else:
+        return render_template('index.html', register=True)
 
 
-if __name__ =='__main__':
+@app.route('/schedule/', methods=['post', 'get'])
+def schedule_app():
+    global schedule
+    global current_user
+    attempted_week = Week()
+    print(attempted_week)
+    restaurant = False
+    date = False
+    if request.method == 'POST':
+        restaurant = request.form.get('restaurant')
+        date = request.form.get('date')
+
+    if restaurant and date:
+        print(restaurant)
+        print(date)
+
+    return render_template('app.html', username=current_user.name)
+
+
+@app.route('/logout/')
+def logout():
+    global current_user
+    current_user = False
+    return render_template('logout.html')
+
+
+if __name__ == '__main__':
     app.run()
